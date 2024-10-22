981b177683fefc6aa39db0a83d3ac161649e317a
CodeGallantX <johnayobami77@proton.me>
2024-10-22 18:28:47 +0100

    added more pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/home";
 import Cart from "./pages/cart";
 import Checkout from "./pages/checkout";
+import Account from "./pages/account";
+import WishList from "./pages/wishlist";
 import Contact from "./pages/contact";
 
 
@@ -12,6 +14,8 @@ const App = () => {
       <Route path="/" element={<Home/>}/>
       <Route path="/cart" element={<Cart/>}/>
       <Route path="/checkout" element={<Checkout/>}/>
+      <Route path="/account" element={<Account/>}/>
+      <Route path="/wishlist" element={<WishList/>}/>
       <Route path="/contact" element={<Contact/>}/>
     </Routes>
   )
